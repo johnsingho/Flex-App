97c60ceb31a2251b9a7b20a11b7f60b471314cf1
johnsing <johnsing.he@flex.com>
2018-08-06 17:00:18 +0800

    back

--- www/js/index.js
+++ www/js/index.js
@@ -30,8 +30,9 @@ var app = {
 //            alert(error.description);
         } else {
 //            alert('Update is loaded');
+            chcp.installUpdate(this.installationCallback);
         }
-        chcp.installUpdate(this.installationCallback);
+        
     },
 
     onUpdateReady: function(eventData) {
